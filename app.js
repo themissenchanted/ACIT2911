29de61085963ec98b559eaddfae8ae32d0037503
Taylor Artemenko <taylorartemenko6@gmail.com>
2019-05-05 16:47:10 -0700

    New Version - Included: Register and Login with error checking, logout button, session cookies for eventual push to the cloud, and some other stuff

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const utils = require('./utils.js');
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
+const cart_string = "'s Cart";
+const session = require('express-session');
 
 var app = express();
 const router = express.Router();
@@ -18,11 +20,36 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  name: "nozamA",
+  resave: false,
+  saveUninitialized: false,
+  secret: "ssh!quiet,it\'asecret!'",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 60 * 2,
+    sameSite: true,
+  }
+}));
+
+const redirectCart = (req, res, next) => {
+  if (!req.session.username) {
+    res.render("landing.hbs", {
+      loginlogoutButton: `<li class="nav-item" id="loginbutton">
+      <a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a>
+      </li>`
+    });
+  } else {
+    next()
+  }
+};
+
 router.get('/', (request, response) => {
-  response.render("landing.hbs");
+  response.render("landing.hbs", {
+    loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>'
+  });
 });
 
-router.get('/cart', (request, response) => {
+router.get('/cart', redirectCart, (request, response) => {
   response.sendFile(path.join(__dirname+ "/cart.html"));
 });
 
@@ -54,32 +81,59 @@ app.post('/login', (request, response) => {
     for (i=0; i < result.length; i++) {
       if (request.body.username === result[i].username) {
         if (request.body.password === result[i].password) {
-          response.render('index.hbs', {
-            success_login: 'You are logged in!'
+          request.session.username = request.body.username;
+          request.session.cart = result[i].cart;
+          response.render('landing.hbs', {
+            cartLink: `<li class="nav-item" id="cart"><a href="http://localhost:8080/cart" class="nav-link">${request.body.username + cart_string}</a></li>`,
+            loginlogoutButton: '<li class="nav-item" id="cart"><a href="http://localhost:8080/logout" class="nav-link">Logout</a></li>',
           })
         } else {
-          response.render('index.hbs', {
-            success_login: 'Invalid login info'
-          })
+          response.render('landing.hbs', {
+            popup: "<script>alert(\'Invalid Login Information, try again!'</script>",
+            loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>'
+          });
         }
       } else {
-        response.render('index.hbs', {
-          success_login: 'Invalid login info'
-        })
+        response.render('landing.hbs', {
+          popup: '<script>alert("Invalid Login Information, try again!")</script>',
+          loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>'
+        });
       }
     }
   });
 });
 
-app.post('/register', function (req, res) {
+app.post('/register', function (request, response) {
   var db = utils.getDb();
-  req.body.cart = [];
-  db.collection('users').insertOne(req.body);
-  res.render("landing.hbs", {
-    cartLink: "<li class=\"nav-item\" id=\"cart\">\n" +
-        "<a href=\"http://localhost:8080/cart\" class=\"nav-link\">Your Cart</a>\n" +
-        "</li>"
+  request.body.cart = [];
+  db.collection('users').find({username: `${request.body.username}`}).toArray().then(function (result) {
+    if (result.length === 0) {
+      db.collection('users').insertOne(request.body);
+      request.session.username = request.body.username;
+      request.session.cart = request.body.cart;
+      response.render('landing.hbs', {
+        cartLink: `<li class="nav-item" id="cart"><a href="http://localhost:8080/cart" class="nav-link">${request.session.username + cart_string}</a></li>`,
+        loginlogoutButton: '<li class="nav-item" id="cart"><a href="http://localhost:8080/logout" class="nav-link">Logout</a></li>'
+      });
+    } else {
+      response.render('landing.hbs', {
+        popup: '<script>alert(\'Account already exists with that username, try again!\')</script>\n',
+        loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>'
+      });
+    }
   });
+});
+
+app.get('/logout', (request, response) => {
+  request.session.destroy(err => {
+    if (err) {
+      response.render('landing.hbs');
+    }
+    response.clearCookie("nozamA");
+    response.render("landing.hbs", {
+      loginlogoutButton: '<li class="nav-item" id="loginbutton"><a href="#" class="nav-link" data-toggle="modal" data-target="#login">Login</a></li>'
+    });
+  })
 });
 
 module.exports = server;
