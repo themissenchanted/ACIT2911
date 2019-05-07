01d6c5b1159d1aa4e4f4bfcb6c23e7f309d0d06e
Raj123Kaur <49548644+Raj123Kaur@users.noreply.github.com>
2019-05-07 15:39:21 -0700

    nn
    vcbnb

--- landing.js
+++ landing.js
@@ -34,3 +34,33 @@ function reveal() {
     eye.className = "fa fa-eye"
   }
 }
+
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+/* Open when someone clicks on the span element */
+function openFooter() {
+  document.getElementById("myFooter").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeFooter() {
+  document.getElementById("myFooter").style.width = "0%";
+}
